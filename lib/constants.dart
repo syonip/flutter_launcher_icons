c3e1fab4f80519764625aa69f62c13161a40fd12
syonip <syonip@gmail.com>
2021-12-02 12:50:02 +0200

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,18 +1,21 @@
-String androidResFolder(String? flavor) => "android/app/src/${flavor ?? 'main'}/res/";
-String androidColorsFile(String? flavor) => "android/app/src/${flavor ?? 'main'}/res/values/colors.xml";
+String androidResFolder(String? flavor) =>
+    "android/app/src/${flavor ?? 'main'}/res/";
+String androidColorsFile(String? flavor) =>
+    "android/app/src/${flavor ?? 'main'}/res/values/colors.xml";
 const String androidManifestFile = 'android/app/src/main/AndroidManifest.xml';
 const String androidGradleFile = 'android/app/build.gradle';
 const String androidFileName = 'ic_launcher.png';
 const String androidAdaptiveForegroundFileName = 'ic_launcher_foreground.png';
 const String androidAdaptiveBackgroundFileName = 'ic_launcher_background.png';
-String androidAdaptiveXmlFolder(String? flavor) => androidResFolder(flavor) + 'mipmap-anydpi-v26/';
+String androidAdaptiveXmlFolder(String? flavor) =>
+    androidResFolder(flavor) + 'mipmap-anydpi-v26/';
 const String androidDefaultIconName = 'ic_launcher';
 
 const String iosDefaultIconFolder =
-    'ios/Runner/Assets.xcassets/AppIcon.appiconset/';
-const String iosAssetFolder = 'ios/Runner/Assets.xcassets/';
-const String iosConfigFile = 'ios/Runner.xcodeproj/project.pbxproj';
-const String iosDefaultIconName = 'Icon-App';
+    'ios/Pickapp Cordova/Images.xcassets/AppIcon.appiconset/';
+const String iosAssetFolder = 'ios/Pickapp Cordova/Images.xcassets/';
+const String iosConfigFile = 'ios/Pickapp Cordova.xcodeproj/project.pbxproj';
+const String iosDefaultIconName = 'icon';
 
 const String errorMissingImagePath =
     'Missing "image_path" or "image_path_android" + "image_path_ios" within configuration';
